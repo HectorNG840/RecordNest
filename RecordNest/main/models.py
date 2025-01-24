9e3c93444730746ab9c06f160bb21adb11f688ae
HectorNG840 <hecnoggon@alum.us.es>
2025-01-24 11:53:32 +0100

    Feat: Make main models

--- RecordNest/main/models.py
+++ RecordNest/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Collection(models.Model):
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE,
+                                related_name='collection')
+    lenght = models.IntegerField()
+    creation_date = models.DateField()
+    records = models.ManyToManyField('records.Record',
+                                     related_name='collections')
+
+    def __str__(self):
+        return self.lenght
